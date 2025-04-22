935d26e7c53697a09b6e9d675107f0252f0f0e95
alexandre.vinent <alexandre.vinent@estudiantat.upc.edu>
2025-04-22 04:34:28 +0200

    I am the Bone of my Sword Steel is my Body and Fire is my Blood. I have created over a Thousand Blades, Unknown to Death, Nor known to Life. Have withstood Pain to create many Weapons Yet those Hands will never hold Anything. So, as I Pray-- Unlimited Blade Works

--- FONTS/src/main/Presentacion/Drivers/DriverGestionJuego.java
+++ FONTS/src/main/Presentacion/Drivers/DriverGestionJuego.java
@@ -212,7 +212,7 @@ public class DriverGestionJuego {
                             true);
                     while (true) { 
                         menuPartida();
-                    }}
+                    }
                     
                 }
                
